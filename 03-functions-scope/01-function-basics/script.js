c426834df03d51b06a9639bbc52502a8322f69c2
enwadiogbu <emekanwadiogbu.jr@me.com>
2024-10-22 17:10:54 -0700

    relearning the scope of a function

---
+++ 03-functions-scope/01-function-basics/script.js
@@ -0,0 +1,20 @@
+function sayHello() {
+    console.log('Hello world!');
+}
+
+sayHello(); //calling the function
+
+function add(num1, num2) {
+    console.log(num1 + num2); //defining or declaring the function
+}
+
+add(5, 10); //passing the arguments
+
+function subtract(num1, num2) {
+    return num1 - num2;
+    
+}
+
+const result = subtract(10,2);
+
+console.log(result, subtract(25,45));
